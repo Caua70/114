42e654b6fb6f2aa217022bd07a0fc92dfc9d4129
Caua70 <101745397+Caua70@users.noreply.github.com>
2023-05-10 16:25:36 -0300

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,14 @@
+a = 33
+b = "33"
+
+if int(b) > a:
+    print("b é maior que a")
+elif int(b) == a:
+    print("a e b são iguais")
+
+def my_function(fname):
+    print(fname + " refness")
+
+my_function("Emily")
+my_function("Tobias")
+my_function("Lucas")
